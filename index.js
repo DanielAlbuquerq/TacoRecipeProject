d1e23aa23273e56b841b31b6fb408160013d0521
Daniel Mendes de Albuquerque <daniel_developer@hotmail.com>
2023-10-06 01:02:03 -0300

    express public done

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 let data;
 
 app.get("/", (req, res) => {
-  res.render("myIndex.ejs",{recipe: data});
+  res.render("index.ejs",{recipe: data});
 });
 
 
